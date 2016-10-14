97dd9ab600a70da06b83ca864985db63549d1db0
Ashmeet Singh <ashmeet.singh@goibibo.com>
2016-10-15 03:12:47 +0530

    New changes

--- hackathon2016/foodnrestaurant/urls.py
+++ hackathon2016/foodnrestaurant/urls.py
@@ -10,8 +10,8 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^test/$','foodnrestaurant.views.test'),
-    url(r'^getmenu/(?P<hotelid>\w+)$','foodnrestaurant.views.get_menu'),
-    url(r'^placeorder/(?P<FMN>\w+)$','foodnrestaurant.views.place_order'),
-    url(r'^getorder/(?P<FMN>\w+)$','foodnrestaurant.views.get_order'),
+    url(r'^getmenu/(?P<hotelid>\w+)/$','foodnrestaurant.views.get_menu'),
+    url(r'^placeorder/(?P<FMN>\w+)/$','foodnrestaurant.views.place_order'),
+    url(r'^getorder/(?P<FMN>\w+)/$','foodnrestaurant.views.get_order'),
 
 )
